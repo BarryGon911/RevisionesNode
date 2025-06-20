cb7b7310841bcb4c0bbfa4ecb1e04cff0789bce1
BarryGon911 <corragio.sage@gmail.com>
2025-06-20 15:59:40 -0600

    Changes

--- primo-checker/primo.js
+++
@@ -1,9 +0,0 @@
-export function esPrimo(n) {
-  if (n <= 1) return false;
-
-  const limite = Math.sqrt(n);
-  for (let i = 2; i <= limite; i++) {
-    if (n % i === 0) return false;
-  }
-  return true;
-}
