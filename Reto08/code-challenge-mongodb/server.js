f673259889f8b0884295dfdbe86e2b4f4e8a7bf1
BarryGon911 <corragio.sage@gmail.com>
2025-08-09 13:44:40 -0600

    changes

--- Reto08/code-challenge-mongodb/server.js
+++ Reto08/code-challenge-mongodb/server.js
@@ -1,8 +1,9 @@
 import express from "express";
-import dotenv from "dotenv";
 import rutas from "./src/routes/index.js";
 import connectDB from "./src/config/database.js";
+import colors from "colors";
 
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
@@ -10,13 +11,29 @@ app.use(express.json());
 
 app.use("/", rutas);
 
-const port = process.env.SRV_PORT || 3000;
+const port = process.env.SRV_PORT || 4000;
 
-connectDB().then(() => {
-  app.listen(port, () => {
-    console.log(`Servidor escuchando en puerto ${port}`);
-  });
-}).catch(err => {
-  console.error("Fallo al conectar la DB:", err);
-  process.exit(1);
+// Método de Rodrigo
+
+// connectDB().then(() => {
+//   app.listen(port, () => {
+//     console.log(`Servidor escuchando en puerto ${port}`);
+//   });
+// }).catch(err => {
+//   console.error("Fallo al conectar la DB:", err);
+//   process.exit(1);
+// });
+
+(async () => {
+  try {
+    await connectDB();
+    console.log(colors.bgBlue.cyan.italic.bold("🟢 Conexión a la BD exitosa"));
+  } catch (error) {
+    console.error(colors.bgRed.white.bold("🔴 La Conexión a la BD ha fallado:"), colors.red(error instanceof Error ? error.message : String(error)));
+    process.exit(1);
+  }
+})();
+
+app.listen(port, () => {
+  console.log(colors.bgMagenta.magenta.italic.bold(`🚀 Servidor ejecutándose en http://localhost:${port}`));
 });
