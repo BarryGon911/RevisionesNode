05569427846d2f31b22e438a396810ed3343a8c6
BarryGon911 <corragio.sage@gmail.com>
2025-08-20 22:48:34 -0600

    changes

--- Retos/Reto09/code-challenge-mysql/queries/poblar_db.sql
+++ Retos/Reto09/code-challenge-mysql/queries/poblar_db.sql
@@ -1,19 +1,23 @@
--- AUTOR
-INSERT INTO Autors (id, nombre, nacionalidad, fechaNacimiento, createdAt, updatedAt)
+-- AUTORES
+INSERT INTO Autor (id, nombre, nacionalidad, fechaNacimiento)
 VALUES 
-  (1, 'Gabriel García Márquez', 'Colombiana', '1927-03-06', NOW(), NOW()),
-  (2, 'Isabel Allende', 'Chilena', '1942-08-02', NOW(), NOW());
+  (1, 'Gabriel García Márquez', 'Colombiana', '1927-03-06'),
+  (2, 'Isabel Allende', 'Chilena', '1942-08-02');
+
+-- USUARIOS
+INSERT INTO Usuario (id, nombre, email, password)
+VALUES
+  (1, 'Juan Camaney Shingón', 'jcamaney@mail.com', 'misuperpassword1'),
+  (2, 'Mauricio Garcés Elú', 'mgarces@mail.com', 'misuperpassword2');
 
 -- LIBROS
-INSERT INTO Libros (id, titulo, anio, genero, autorId, createdAt, updatedAt)
+INSERT INTO Libro (id, titulo, genero, fechaPublicacion, autorId)
 VALUES 
-  (1, 'Cien Años de Soledad', 1967, 'Realismo Mágico', 1, NOW(), NOW()),
-  (2, 'El Amor en los Tiempos del Cólera', 1985, 'Romance', 1, NOW(), NOW()),
-  (3, 'La Casa de los Espíritus', 1982, 'Ficción', 2, NOW(), NOW());
+  (1, 'Cien Años de Soledad', 'Realismo Mágico', '1967', 1),
+  (2, 'El Amor en los Tiempos del Cólera', 1985, 'Romance', 2);
 
 -- RESEÑAS
-INSERT INTO Resenas (id, comentario, puntuacion, fecha, libroId, createdAt, updatedAt)
+INSERT INTO Resena (id, contenido, calificacion, fecha, libroId, usuarioId)
 VALUES 
-  (1, 'Una obra maestra', 5, '2025-07-01', 1, NOW(), NOW()),
-  (2, 'Emotiva y profunda', 4, '2025-07-05', 2, NOW(), NOW()),
-  (3, 'Muy imaginativa', 5, '2025-07-10', 3, NOW(), NOW());
+  (1, 'Una obra maestra', 5, '2025-07-01', 1, 1),
+  (2, 'Muy imaginativa', 5, '2025-07-10', 2, 2);
