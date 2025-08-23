29f2d28b3af378392a0985ca4439282b26037b7c
BarryGon911 <corragio.sage@gmail.com>
2025-08-23 10:34:46 -0600

    changes

--- ecommerce-api/src/utils/pagination.js
+++
@@ -1,9 +0,0 @@
-export function paginateQuery(query, { page = 1, limit = 10 }) {
-  const skip = (page - 1) * limit;
-  return { skip, limit };
-}
-
-export function buildPaginatedResult({ data, total, page, limit }) {
-  const pages = Math.ceil(total / limit) || 1;
-  return { total, page, pages, data };
-}
